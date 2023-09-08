750484a014cff07e91089aac4e6592811275dbdc
QuackingDuck57 <williamharte622@gmail.com>
2023-09-08 09:03:17 -0700

    stuff

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,8 +3,19 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Chronometer
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var startMain : Button
+    lateinit var stopMain : Button
+    lateinit var resetMain : Button
+    lateinit var watchMain : Chronometer
+    lateinit var layout : ConstraintLayout
+
 
     //public static final double PI = 3.14   declaring a classwide constant in java
     //in kotlin, we use a companion object
@@ -17,7 +28,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
         Log.d(TAG, "onCreate: this is a log")
+        watchMain.freezesText = true
+        startMain.setOnClickListener {
+            watchMain.freezesText = false
+            watchMain.start()
+            startMain.visibility = View.INVISIBLE
+            stopMain.visibility = View.VISIBLE
+            stopMain.setOnClickListener {
+                watchMain.stop()
+                watchMain.freezesText = true
+                stopMain.visibility = View.INVISIBLE
+                startMain.visibility = View.VISIBLE
+            }
+        }
+        resetMain.setOnClickListener {
+            //watchMain resets
+        }
+
+
     }
 
     //to override an existing function, just start typing it
@@ -25,6 +55,19 @@ class MainActivity : AppCompatActivity() {
     //function that exists in the superclass)
     override fun onStart(){
         super.onStart()
+    }
+    override fun onRestart(){
+        super.onRestart()
+    }
+    override fun onResume(){
+        super.onResume()
+    }
+    fun wireWidgets(){
+        startMain = findViewById(R.id.start_button_main)
+        stopMain = findViewById(R.id.stop_button_main)
+        resetMain = findViewById(R.id.reset_button_main)
+        watchMain = findViewById(R.id.chronometer_main_stopwatch)
+        layout = findViewById(R.id.layout_main)
     }
 
 }
